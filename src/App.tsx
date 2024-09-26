c720f27fbd9d6a6a04d079a44d99a8b32114b529
BeHumanX <loxocker@gmail.com>
2024-09-26 20:40:43 +0700

    Add Upgrade Insecure Request

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import { useState } from "react";
 import { Button } from "./components/Button";
 import { Header } from "./components/Header";
-import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import Home from "./pages/Home";
 
 function App() {
-  
-
   return (
     <>
+      <Helmet>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
+      </Helmet>
       <Router>
-        <Header onCreateAccount={() => {}} onLogin={() => {}} onLogout={() => {}} user={{name:"Jane"}} />
+        <Header
+          onCreateAccount={() => {}}
+          onLogin={() => {}}
+          onLogout={() => {}}
+          user={{ name: "Jane" }}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
       {/* <Button primary label="click me :3"  size="small" /> */}
     </>
-  );  
+  );
 }
 
 export default App;
